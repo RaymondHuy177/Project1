c2012caddbba515f240dbe243cba204e232b2ea1
RaymondHuy177 <luongvanhuy177@gmail.com>
2015-07-11 21:40:57 -0700

    Update Logo.cs

--- Logo.cs
+++ Logo.cs
@@ -16,7 +16,7 @@ namespace WebPhoHienTravel.App_Code
 {
     public class _Logo
     {
-//ac
+//acdef
         private string masterPath;
         private WebPhoHienTravel.App_Code.DATACONNECT data;
         public _Logo(string masterPath)
